8db0f81b12f1a2a8ea9594c3c1ab701502c6ce6c
DaniTomicic <dani.tomicic@ikasle.egibide.org>
2025-02-14 08:47:25 +0100

    jugador update

--- Proyecto/Codigo/src/Modelo/JugadorDAO.java
+++ Proyecto/Codigo/src/Modelo/JugadorDAO.java
@@ -1,7 +1,27 @@
 package Modelo;
 
+import Controlador.JugadorController;
+
+import java.util.ArrayList;
+
 public class JugadorDAO {
-    public static void actualizarJugador(Jugador jugadorM) {
+    public static ArrayList<Jugador> listaJugadores;
+
+    public static void agregarJugador(Jugador jugador) {
+        listaJugadores.add(jugador);
 
     }
+    public static void altaJugador() {
+        boolean continuar = false;
+        do {
+            try {
+                Jugador jugador = JugadorController.solicitarValidarDatos();
+                JugadorDAO.agregarJugador(jugador);
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } while (continuar);
+    }
+
 }
